7a3783fe45d9d4e64c578136cd5ef07039408cf5
amutatsu <amutatsu007@gmail.com>
2019-12-11 07:17:40 +0900

    Initial Commit

---
+++ weatherApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  weatherApp
+//
+//  Created by Tatsuya Amida on 2019/12/11.
+//  Copyright © 2019 Tatsuya Amida. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
